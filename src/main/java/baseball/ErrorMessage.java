a496e68655e3986adb236c54f8fe09d9f2cb4592
hjhan <hjhan.0621@gmail.com>
2023-10-29 04:56:17 +0900

    📍refactor: ErrorMessage > 변수 접근제어자 public static final으로 변경

--- src/main/java/baseball/ErrorMessage.java
+++ src/main/java/baseball/ErrorMessage.java
@@ -1,7 +1,7 @@
 package baseball;
 
 public class ErrorMessage {
-    private final String ERROR_WRONG_USER_NUMBER = "1부터 9사이의 숫자로 이루어진, 중복되지 않는 숫자 세자리를 입력하세요.";
-    private final String ERROR_WRONG_SELECTED_VALUE = "숫자 '1' 또는 '2'를 입력하세요.";
+    public static final String ERROR_WRONG_USER_NUMBER = "1부터 9사이의 숫자로 이루어진, 중복되지 않는 숫자 세자리를 입력하세요.";
+    public static final String ERROR_WRONG_SELECTED_VALUE = "숫자 '1' 또는 '2'를 입력하세요.";
 
 }

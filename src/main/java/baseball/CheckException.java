fde51dc67e8046d85fcba3654b32c66638a43482
hjhan <hjhan.0621@gmail.com>
2023-10-29 04:56:55 +0900

    refactor: CheckException > ErrorMessage 변경사항 적용

--- src/main/java/baseball/CheckException.java
+++ src/main/java/baseball/CheckException.java
@@ -1,18 +1,21 @@
 package baseball;
 
+import static baseball.ErrorMessage.ERROR_WRONG_SELECTED_VALUE;
+import static baseball.ErrorMessage.ERROR_WRONG_USER_NUMBER;
+
 public class CheckException {
     private final String USER_NUM_REG_EX = "([1-9])(?!\\1)([1-9])(?!\\1|\\2)([1-9])";
     private final String END_OR_NOT = "[12]";
 
     public void guessNumberException(String st) {
         if (!st.matches(USER_NUM_REG_EX)) {
-            throw new IllegalArgumentException(Narrator.USER_NUM_REG_EX);
+            throw new IllegalArgumentException(ERROR_WRONG_USER_NUMBER);
         }
     }
 
     public void endNumberException(String st) {
         if (!st.matches(END_OR_NOT)) {
-            throw new IllegalArgumentException(Narrator.END_OR_NOT);
+            throw new IllegalArgumentException(ERROR_WRONG_SELECTED_VALUE);
         }
     }
 }

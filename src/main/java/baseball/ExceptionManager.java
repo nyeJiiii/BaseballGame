dec7b55f595f5f6fc078559e37e6e19bd7e5a64c
hjhan <hjhan.0621@gmail.com>
2023-10-29 05:50:56 +0900

    📍refactor: CheckException > ExceptionManager 클래스 이름 변경, 싱글톤으로 구현

--- src/main/java/baseball/ExceptionManager.java
+++ src/main/java/baseball/ExceptionManager.java
@@ -3,9 +3,20 @@ package baseball;
 import static baseball.ErrorMessage.ERROR_WRONG_SELECTED_VALUE;
 import static baseball.ErrorMessage.ERROR_WRONG_USER_NUMBER;
 
-public class CheckException {
+public class ExceptionManager {
+
+    private static ExceptionManager exceptionManager;
     private final String RIGHT_USER_NUMBER = "([1-9])(?!\\1)([1-9])(?!\\1|\\2)([1-9])";
     private final String RIGHT_SELECTED_VALUE = "[12]";
+
+    private ExceptionManager() {}
+
+    public static ExceptionManager getInstance() {
+        if (exceptionManager == null) {
+            exceptionManager = new ExceptionManager();
+        }
+        return exceptionManager;
+    }
 
     public void checkWrongUserNumberException(String st) {
         if (!st.matches(RIGHT_USER_NUMBER)) {
